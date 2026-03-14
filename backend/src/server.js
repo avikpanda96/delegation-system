a6896f4927ba45545ad982b0f21b156dea27c402
Avik Panda <98453311+avikpanda96@users.noreply.github.com>
2026-03-14 12:14:50 +0530

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,4 @@
 const app = require("./app");
-const pool = require("./config/db");
-// Root route (to test Render URL)
-app.get("/", (req, res) => {
-  res.send("Backend is live ✅");
-});
-
-// Test DB connection route
-app.get("/test-db", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT 1 + 1 AS result");
-    res.json({ success: true, result: rows });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
-  }
-});
 
 app.listen(5000, () => {
   console.log("Server running");
