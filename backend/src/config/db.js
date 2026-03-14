6cc4e7606dbf344aa47c8f0aa7d335aa109e372a
Avik Panda <98453311+avikpanda96@users.noreply.github.com>
2026-03-14 11:35:38 +0530

    Add files via upload

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,16 +2,17 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST,          
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Use the port from Railway
+  port: process.env.DB_PORT || 3306,   
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false // REQUIRED for Railway/Aiven/Render
+    
+    rejectUnauthorized: false
   }
 });
 
